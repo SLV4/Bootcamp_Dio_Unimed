1f1b435e29e088818f0a3f9fce4d7e1d1fc66694
SLV <sant_andrey@live.com>
2022-10-13 17:51:21 -0300

    adicionado exercício convertendo_tipos_dados

---
+++ python_exercicios/convertendo_tipos_dados.py
@@ -0,0 +1,12 @@
+print (int(1.1687654))
+print (int("10"))
+print (float(10.10))
+print(float(100))
+
+valor = 10
+valor_str = str(valor)
+print (type(valor))
+print(type(valor_str))
+
+print(10 / 2)
+print(10 // 2)

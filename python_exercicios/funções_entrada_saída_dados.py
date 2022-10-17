17d993e3da5fb06343a5ed83504480e2767c1327
SLV <sant_andrey@live.com>
2022-10-17 11:35:33 -0300

    adição_aula_input_print

---
+++ python_exercicios/funções_entrada_saída_dados.py
@@ -0,0 +1,10 @@
+#objetivo: ler valores com a função input
+#imprimir resultados com a função print
+nome = input("informe o seu nome: ")
+idade = input("informe a sua idade: ")
+print(nome, idade)
+print(nome, idade, end= "...\n")
+print(nome, idade, sep= "#")
+print(nome, idade, sep= "#", end="...\n")
+
+
